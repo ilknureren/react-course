8d0815dc0afeb57c9d11bee52cec5070cc9c8fcf
ilkxeren <ilkxeren@gmail.com>
2020-09-15 14:43:38 -0400

    video 1-9 done

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+const StorePicker = () => (
+        <form className="store-selector">
+            <h2> Please Enter A Store</h2>
+            {/* <input type="text" required>Store Name</input> */}
+            <button type="submit">Visit Store</button>
+        </form>
+        )
+    
+
+
+export default StorePicker;
